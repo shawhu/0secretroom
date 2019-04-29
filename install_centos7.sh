57b4d9555b3f5b014ce27ce889695de602685942
ericgu2017 <49860926+ericgu2017@users.noreply.github.com>
2019-04-29 09:52:03 +0800

    Update install_centos7.sh

--- install_centos7.sh
+++ install_centos7.sh
@@ -1,15 +1,19 @@
 #install file for CentOS 7
 #yum update
+echo "我们推荐先跑一边yum update更新一下系统"
 echo "Let's run yum update first? Highly recommended for a new system."
 echo ""
+echo "输入Y/y开始，输入空格取消"
 read -p "Enter Y or y to start, anything else to bypass." -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     yum update -y
 fi
-echo "Now we are going to start the installation, it takes several minutes and can't be interupted."
+echo "安装正式开始，大概需要几分钟时间中间不能打断"
+echo "Now we are going to start the installation, it takes several minutes and can't be interupted"
 echo 
+echo "按Y/y开始，其他键取消"
 read -p "Shall we start the process? Enter Y or y to start, anything else to quit." -n 1 -r
 echo    # (optional) move to a new line
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -49,14 +53,26 @@ systemctl start nginx
 semanage permissive -a httpd_t
 
 #generate self-signed cert
-echo "We will generate a self-signed ssl certificate now..."
+echo "接下来要系统要产生一个自签名用于https加密的ssl证书，请根据提示输入一些信息。可能是英语界面"
+echo "Common name请输入0secretroom.local"
+echo "We will generate a self-signed ssl certificate, please follow the instruction to enter various information"
+echo "Common name must be 0secretroom.local"
+echo "按任意键开始"
+read -p "Press anykey to continue" -n 1 -r
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/certs/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
 cp 0secretroom/my.conf /etc/nginx/conf.d/my.conf
 nginx -s reload
 
 #output results
-echo -e "All done, you should add   \e[34m192.168.1.1 0secretroom.local\e[0m   to your local hosts file and"
-echo -e "then you shall be able to access by entering the following in your browser \e[34mhttps://0secretroom.local/#/wschat?rid=aaa&uid=yourUsername\e[0m"
+#get local ip
+myip=`hostname -I`
+echo -e "All done, you should add  \e[34m$myip 0secretroom.local\e[0m  to /etc/hosts and"
+echo -e "you shall be able to access by entering the following in your browser \e[34mhttps://0secretroom.local/#/wschat?rid=aaa&uid=yourUsername\e[0m"
 echo -e "Sample URL: \e[34mhttps://0secretroom.local/#/wschat?rid=aaa&uid=harry\e[0m"
 echo -e "Please check-out our project site for details: \e[34mhttps://github.com/ericgu2017/0secretroom\e[0m"
+echo -e "请自行把 \e[34m$myip 0secretroom.local\e[0m  添加到/etc/hosts文件中"
+echo -e "你可以试下访问 \e[34mhttps://0secretroom.local/#/wschat?rid=aaa&uid=随便起一个管理员用户名\e[0m"
+echo -e "举例： \e[34mhttps://0secretroom.local/#/wschat?rid=aaa&uid=harry\e[0m"
+echo -e "如有问题请访问项目网站 \e[34mhttps://github.com/ericgu2017/0secretroom\e[0m"
+
 
