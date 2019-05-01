e8def3e42992daac0c0d5f71c663be39a57e1897
ericgu2017 <49860926+ericgu2017@users.noreply.github.com>
2019-05-01 13:54:20 +0800

    Update install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -1,9 +1,8 @@
 #install file for Ubuntu 16
 echo "0SecretRoom easy install Ver: 1.0"
-echo "********************************************************************"
+echo "**********************************************************************"
 echo "如服务器在中国推荐使用mirrors.aliyun.com，是否允许我们调整/etc/apt/sources.list?"
 echo "If the server is in China, shall we switch /etc/apt/sources.list to use mirrors.aliyun.com?"
-echo "********************************************************************"
 echo 
 echo "输入Y/y开始，其他键取消, Enter Y/y to switch, anything else to bypass"
 read -p "Enter Y or y to start, anything else to bypass." -n 1 -r
@@ -13,22 +12,21 @@ then
     sudo sed -i 's/archive.ubuntu/mirrors.aliyun/g' /etc/apt/sources.list
     echo "apt源已经替换成aliyun, apt repo has been switched to use aliyun"
 fi
-echo "********************************************************************"
+echo "**********************************************************************"
 echo "推荐先跑一遍apt update更新一下系统"
 echo "Shall we run apt update for you first? Highly recommended for a new system."
-echo "********************************************************************"
 echo ""
 echo "输入Y/y开始，其他键取消"
 read -p "Enter Y or y to start, anything else to bypass." -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    sudo apt update -y && sudo apt upgrade -y
+    sudo apt update -y
+    sudo apt upgrade -y
 fi
-echo "********************************************************************"
+echo "**********************************************************************"
 echo "安装正式开始，大概需要几分钟时间中间不能打断"
 echo "Now we are going to start the installation, it takes several minutes and it can't be interupted"
-echo "********************************************************************"
 echo 
 echo "按Y/y开始，其他键取消"
 read -p "Enter Y or y to start, anything else to quit." -n 1 -r
@@ -70,12 +68,11 @@ sudo systemctl start nginx
 #semanage permissive -a httpd_t
 
 #generate self-signed cert
-echo "********************************************************************"
+echo "**********************************************************************"
 echo "接下来要系统要产生一个自签名用于https加密的ssl证书，请根据英语界面提示输入"
 echo "We will generate a self-signed ssl certificate, please follow the instruction to enter various information"
 echo "Common name请输入0secretroom.local或者你正式域名，该域名指向必须设置到本服务器"
 echo "Common name must be 0secretroom.local or a real domain name that pointed to this server"
-echo "********************************************************************"
 echo 
 echo -ne "请输入网站域名 please enter site domain name: "
 read commonname
@@ -90,7 +87,7 @@ echo "nginx reloaded"
 #output results
 #get local ip
 myip=`hostname -I`
-echo "**********************************************************************************"
+echo "**********************************************************************"
 echo -e "All done, because WebCrypto API only works with https, we took the liberty and we generated a self-signed ssl key"
 echo -e "If you do not have a real domain name you should add  \e[32m$myip $commonname\e[0m  to /etc/hosts and"
 echo -e "try the following link in your browser: \e[32mhttps://$commonname/#/wschat?rid=aaa&uid=testadmin\e[0m"
@@ -100,4 +97,6 @@ echo -e "因此我们在安装过程中产生了一个用于https的自签名ssl
 echo -e "如果你没有正式域名，请自行把 \e[32m$myip $commonname\e[0m  添加到/etc/hosts文件中"
 echo -e "浏览器访问地址:         \e[32mhttps://$commonname/#/wschat?rid=aaa&uid=testadmin\e[0m"
 echo -e "如有问题请访问项目网站:  \e[32mhttps://github.com/ericgu2017/0secretroom\e[0m"
-echo "**********************************************************************************"
+echo 
+echo 
+echo 
